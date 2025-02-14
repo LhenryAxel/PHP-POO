d576134931382f1b53e970f66e79cd23c73a23d5
quentin-pauleau <147131952+quentin-pauleau@users.noreply.github.com>
2025-02-14 16:47:29 +0100

    LA PTN DATE

--- app/Views/listPage.php
+++ app/Views/listPage.php
@@ -117,24 +117,20 @@
         <tbody>
     <?php foreach($pages as $page): ?>
         <tr>
-            <td><?= htmlspecialchars($page['id']) ?></td>
-            <td><?= htmlspecialchars($page['title']) ?></td>
+            <td><?= htmlspecialchars($page->GetId()) ?></td>
+            <td><?= htmlspecialchars($page->GetTitle()) ?></td>
             
             <td>
-                <a href="index.php?page=view&slug=<?= urlencode($page['slug']) ?>">
-                    <?= htmlspecialchars($page['slug']) ?>
+                <a href="index.php?page=view&slug=<?= urlencode($page->GetSlug()) ?>">
+                    <?= htmlspecialchars($page->GetSlug()) ?>
                 </a>
             </td>
             
-            <td><?= htmlspecialchars(strip_tags(substr($page['content'], 0, 300))) ?></td>
-            <td><?= htmlspecialchars($page['history']['creator_email'] ?? 'Unknown') ?></td>
-            <td><?= htmlspecialchars($page['history']['created_at'] ?? 'Unknown') ?></td>
-            <td><?= htmlspecialchars($page['history']['last_modifier_email'] ?? 'None') ?></td>
-            <td><?= htmlspecialchars($page['history']['last_modified_at'] ?? 'Never modified') ?></td>
+            <td><?= htmlspecialchars(strip_tags(substr($page->GetContent(), 0, 300))) ?></td>
             
             <td>
                 <form method="POST" action="index.php?page=delete-page">
-                    <input type="hidden" name="delete_id" value="<?= htmlspecialchars($page['id']) ?>"/>
+                    <input type="hidden" name="delete_id" value="<?= htmlspecialchars($page->GetId()) ?>"/>
                     <button type="submit" name="delete_btn">Supprimer</button>
                 </form> 
             </td>
